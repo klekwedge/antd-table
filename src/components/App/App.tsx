088b38a284d404cd9f370e0e0f11c22f916a129c
Andrey <antonov.andrey.10101@gmail.com>
2023-07-27 18:43:28 +0400

    feat: add basic component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import './App.scss';
+import { IColumn } from '../../types';
+
 function App() {
-  return <div>Hello</div>;
+  const [columns, setColumns] = useState<IColumn[]>([]);
+
+  const addColumn = () => {};
+
+  return (
+    <div className="container">
+      <button className='add-button' type="button" onClick={addColumn}>
+        Добавить
+      </button>
+      <table>
+        <tr>
+          <th>Имя</th>
+          <th>Дата</th>
+          <th>Числовое значение</th>
+        </tr>
+        {
+          columns.map(item => <tr>
+            <td>{item.name}</td>
+            <td>{item.date.toLocaleDateString()}</td>
+            <td>{item.value}</td>
+          </tr>)
+        }
+      </table>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface IColumn {
+    name: string;
+    date: Date;
+    value: number;
+}
